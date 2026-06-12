21fe3cf6a4cd613e6fb7b656068ca6d5d56e6b87
Advita <155878643+advita05@users.noreply.github.com>
2026-06-13 01:40:58 +0530

    Add files via upload

---
+++ Arrays/Left_rotate_the_array_by_one.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotateArrayByOne(vector<int>& nums) {
+        int temp = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            nums[i - 1] = nums[i];
+        }
+        nums[nums.size() - 1] = temp;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 4, 5};
+
+    solution.rotateArrayByOne(nums);
+
+    for (int num : nums) {
+        cout << num << " "; 
+    }
+
+    return 0;
+}
